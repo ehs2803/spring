4de80de82e1c65e911c6fa6e66ed2042613c1bac
ehs2803 <hr2803@naver.com>
2021-10-22 16:20:14 +0900

    20211022

---
+++ spring_mvc/src/main/java/com/example/spring_mvc/basic/HelloData.java
@@ -0,0 +1,10 @@
+package com.example.spring_mvc.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
