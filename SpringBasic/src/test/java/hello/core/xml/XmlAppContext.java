0cec0124d8be9b8c57eff6f5027ee9e132ccf4a6
ehs2803 <hr2803@naver.com>
2022-01-03 09:58:50 +0900

    20210103-bean

--- SpringBasic/src/test/java/hello/core/xml/XmlAppContext.java
+++ SpringBasic/src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,14 +1,14 @@
-package hello.core;
+package hello.core.xml;
 
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import static org.assertj.core.api.Assertions.*;
-public class SpringBasicApplicationTests {
+public class XmlAppContext {
     @Test
     void xmlAppContext() {
-        ApplicationContext ac = new
+        GenericXmlApplicationContext ac = new
                 GenericXmlApplicationContext("appConfig.xml");
 
         MemberService memberService = ac.getBean("memberService",
