20ef1caf310395d4d6cd300c952e89a83c7edeb9
ehs2803 <hr2803@naver.com>
2022-01-06 14:06:57 +0900

    20220106-bean

--- SpringBasic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ SpringBasic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,22 +6,24 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+//@RequiredArgsConstructor // final 필드 생성자 만들어줌.
 public class OrderServiceImpl implements OrderService{
 
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy; // 구체 클래스 의존관계 없앰.
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // 구체 클래스 의존관계 없앰.
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired // 생성자 한개 있을 때 생략가능
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
         this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
+        this.discountPolicy = rateDiscountPolicy;
     }
 
     @Override
