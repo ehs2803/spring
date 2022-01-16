c921419afc74a03fffc484389227764ae2efe53f
ehs2803 <hr2803@naver.com>
2022-01-16 11:50:59 +0900

    20220116-orderweb

--- SpringBootSHOP/src/main/java/jpabook/jpashop/service/OrderService.java
+++ SpringBootSHOP/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -44,7 +46,9 @@ public class OrderService {
     }
 
     // 검색
-    //public List<Order> findOrders(OrderSearch orderSearch){
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByString(orderSearch);
+    }
 
     //}
 }
